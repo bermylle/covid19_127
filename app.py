749a174ae7dd18772136636b1894b73bd3c724a0
bermylle <bermylle@gmail.com>
2020-05-17 21:54:35 +0800

    update

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import flask, csv
+import pymysql
 
 from flask import render_template
 from datasets.ph_data import *
@@ -8,6 +9,7 @@ from datasets.mys_data import *
 from datasets.prk_data import *
 
 app = flask.Flask(__name__)
+
 app.config["DEBUG"] = True
 
 
@@ -33,6 +35,31 @@ def graphs():
 		sgp_deaths_total = sgp_deaths_total, mys_deaths_total = mys_deaths_total, 
 		prk_deaths_total = prk_deaths_total, ph_deaths_total = ph_deaths_total)
 
+class Database:
+    def __init__(self):
+        host = "127.0.0.1"
+        user = "bermyllerazon"
+        password = "L14brmk993014"
+        db = "datasetb"
+        self.con = pymysql.connect(host=host, user=user, password=password, db=db, cursorclass=pymysql.cursors.
+                                   DictCursor)
+        self.cur = self.con.cursor()
+
+    def list_employees(self):
+        self.cur.execute("SELECT * FROM datasetb LIMIT 1")
+        result = self.cur.fetchall()
+        
+        return result
+
+@app.route('/ee')
+def employees():
+    def db_query():
+        db = Database()
+        emps = db.list_employees()
+        return emps
+    res = db_query()
+    
+    return render_template('employees.html', result=res, content_type='application/json')
 
 if __name__ == "__main__":
     app.run(debug=True)
