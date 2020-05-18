dd550b5146bb074841a5be62d8739c2a9eccd2a1
bermylle <bermylle@gmail.com>
2020-05-18 18:14:18 +0800

    fin

--- app.py
+++ app.py
@@ -13,16 +13,16 @@ app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
 #DB
-# app.config['MYSQL_USER'] = ''
-# app.config['MYSQL_PASSWORD'] = ''
-# app.config['MYSQL_HOST'] = ''
-# app.config['MYSQL_DB'] = ''
-# app.config['MYSQL_CURSORCLASS'] = 'DictCursor' #a cursor is a connection to let us run methods for queries. We set it as a dictionary
 app.config['MYSQL_USER'] = ''
 app.config['MYSQL_PASSWORD'] = ''
-app.config['MYSQL_HOST'] = '127.0.0.1'
-app.config['MYSQL_DB'] = 'datasetb'
-app.config['MYSQL_CURSORCLASS'] = 'DictCursor' #a cursor is a connection to let us run methods for queries. We set it as a dictionary
+app.config['MYSQL_HOST'] = 'us-cdbr-east-06.cleardb.net'
+app.config['MYSQL_DB'] = ''
+app.config['MYSQL_CURSORCLASS'] = '' 
+# app.config['MYSQL_USER'] = ''
+# app.config['MYSQL_PASSWORD'] = ''
+# app.config['MYSQL_HOST'] = '127.0.0.1'
+# app.config['MYSQL_DB'] = ''
+# app.config['MYSQL_CURSORCLASS'] = '' 
 mysql = MySQL(app)
 
 @app.route('/')
@@ -52,21 +52,65 @@ def dataseta():
 @app.route('/datasetb')
 def datasetb():
 	#SQL queries
+
+	# [Age Group]
 	cur = mysql.connection.cursor()
-	cur.execute('''SELECT AgeGroup, COUNT(*) AS total_count FROM datasetb GROUP BY AgeGroup ORDER BY AgeGroup''')
-	
+	cur.execute('''SELECT AgeGroup, COUNT(*) AS total_count FROM datasetb GROUP BY AgeGroup ORDER BY AgeGroup DESC''')
 	result = cur.fetchall()
 	
 	AgeGroup = []
 	AgeGroup_Count = []
 
 	for values in result:
-		#print(values["AgeGroup"], values["total_count"])
 		AgeGroup.append(values["AgeGroup"])
 		AgeGroup_Count.append(values["total_count"])
-	print(AgeGroup_Count)
 
-	return render_template('datasetb.html', labels = AgeGroup, data = AgeGroup_Count)
+	# [Sex]
+	cur_sex = mysql.connection.cursor()
+	cur_sex.execute('''SELECT Sex, COUNT(*) AS total_count FROM datasetb GROUP BY Sex;''')
+	result_sex = cur_sex.fetchall()
+
+	sex_data = []
+	sex_data_count = []
+
+	for values in result_sex:
+		sex_data.append(values["Sex"])
+		sex_data_count.append(values["total_count"])
+
+
+	# [RegProvRes]
+	cur_rpr = mysql.connection.cursor()
+	cur_rpr.execute('''SELECT DISTINCT (RegProvRes), COUNT(*) AS total_count FROM datasetb GROUP BY RegProvRes ORDER BY total_count''')
+	result_rpr = cur_rpr.fetchall()
+
+	rpr_data = []
+	rpr_data_count = []
+
+	for values in result_rpr:
+		rpr_data.append(values["RegProvRes"])
+		rpr_data_count.append(values["total_count"])
+
+	# [MuniCityRes]
+	cur_mcr = mysql.connection.cursor()
+	cur_mcr.execute('''SELECT DISTINCT (MuniCityRes), COUNT(*) AS total_count FROM datasetb GROUP BY MuniCityRes ORDER BY total_count''')
+	result_mcr = cur_mcr.fetchall()
+
+	mcr_data = []
+	mcr_data_count = []
+
+	for values in result_mcr:
+		mcr_data.append(values["MuniCityRes"])
+		mcr_data_count.append(values["total_count"])
+
+	# TABLE
+	cur_table = mysql.connection.cursor()
+	cur_table.execute(''' SELECT * FROM datasetb''')
+	result_table = cur_table.fetchall()
+
+	return render_template('datasetb.html', labels = AgeGroup, 
+		data = AgeGroup_Count ,labels_sex = sex_data, data_sex = sex_data_count, 
+		labels_rpr = rpr_data, data_rpr = rpr_data_count, 
+		labels_mcr = mcr_data, data_mcr = mcr_data_count, data_table = result_table)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- static/js/datasetB.js
+++ static/js/datasetB.js
@@ -12,7 +12,7 @@ var config = {
       labels: labels,
       datasets: [
         {
-          label: "Population (millions)",
+          label: "Number of Patients",
           backgroundColor: '#3e95cd',
           data: data
         }
@@ -23,3 +23,84 @@ var config = {
 
 var ctxa = document.getElementById("bar-graph-agegrp");
 var chart = new Chart(ctxa, config);
+
+// doughnut chart [Total # per Sex] 
+var config = {
+   options: {
+      title: {
+        display: true,
+        text: 'Number of patients per Sex'
+      }
+    },
+   type: 'doughnut',
+    data: {
+      labels: labels_sex,
+      datasets: [
+        {
+          label: "Number of Patients",
+          backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9","#c45850"],
+          data: data_sex
+        }
+      ]
+    },
+
+};
+
+var ctxa = document.getElementById("doughnut-chart-sex");
+var chart = new Chart(ctxa, config);
+
+// bar chart [Total # per rpr] 
+var config = {
+   options: {
+      legend: { display: false },
+      title: {
+        display: true,
+        text: 'Number of patients per Region/Province'
+      }
+    },
+    type: 'horizontalBar',
+   data: {
+      labels: labels_rpr,
+      datasets: [
+        {
+          label: "Number of Patients",
+          backgroundColor: "#3e95cd",
+          data: data_rpr
+        }
+      ]
+    },
+
+};
+
+var ctxa = document.getElementById("bar-chart-grouped-location-rpr");
+var chart = new Chart(ctxa, config);
+
+// bar chart [Total # per mcr] 
+var config = {
+   options: {
+      legend: { display: false },
+      title: {
+        display: true,
+        text: 'Number of patients per Municipality/City/Residence'
+      }
+    },
+    type: 'horizontalBar',
+   data: {
+      labels: labels_mcr,
+      datasets: [
+        {
+          label: "Population (millions)",
+          backgroundColor: "#3e95cd",
+          data: data_mcr
+        }
+      ]
+    },
+
+};
+
+var ctxa = document.getElementById("bar-chart-grouped-location-mcr");
+var chart = new Chart(ctxa, config);
+
+$(document).ready( function () {
+    $('#table_data').DataTable();
+} );
